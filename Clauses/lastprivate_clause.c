80b7991db34d53eedf2204e85712df68b0ceac47
Defcon27 <defcon.sentinal95@gmail.com>
2020-08-29 18:05:24 +0530

    last private clause

---
+++ Clauses/lastprivate_clause.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[] = {1, 2, 3, 4, 5};
+    int b[4];
+    int i;
+
+    printf("\nInitial Declaration:\n");
+    for (i = 0; i < 5; i++){
+        printf("%d ", a[i]);
+    }
+
+    #pragma omp parallel
+    {
+        #pragma omp for lastprivate(i)
+        for (i = 0; i < 4; i++)
+        {
+            b[i]=a[i]=a[i+1];
+        }
+    }
+
+    printf("\nArray after construct: \n");
+    for (i = 0; i < 4; i++){
+        printf("%d ", b[i]);
+    }
+
+    return 0;
+}
