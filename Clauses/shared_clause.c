1cfd83a54b5ff0f9557b4da57105c3682739e78f
Defcon27 <defcon.sentinal95@gmail.com>
2020-08-29 17:50:37 +0530

    shared clause

---
+++ Clauses/shared_clause.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[] = {1, 2, 3, 4, 5};
+    int sum = 0;
+    int i = 1;
+
+    printf("\nInitial declaration: Sum is %d", sum);
+
+    #pragma omp parallel shared(sum)
+    {
+        #pragma omp for
+        for (i = 0; i < 5; i++)
+        {
+            sum = sum + a[i];
+        }
+    }
+
+    printf("\n\nOutside construct: Sum is %d", sum);
+
+    return 0;
+}
