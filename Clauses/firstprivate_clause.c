15fb22b943fbee63ffbae5d43797cfb3ed6e85b3
Defcon27 <defcon.sentinal95@gmail.com>
2020-08-29 17:40:42 +0530

    private clause

---
+++ Clauses/firstprivate_clause.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int i=1;
+    int j=2;
+
+    printf("\nInitial declaration: i=%d j=%d",i,j);
+
+    #pragma omp parallel private(i) firstprivate(j)
+    {
+        i=3;
+        j=j+2;
+        printf("\nInside construct: i=%d j=%d",i,j);
+    }
+    
+    printf("\n\nOutside construct: i=%d j=%d",i,j);
+    
+    return 0;
+}
